9aa0cbf452804d53c8a1e1314d5c61810b50b7ec
minseung-gang <strause1@nate.com>
2023-01-02 12:53:47 +0900

    DOCS:카카오 맵 코드 수정

--- src/components/common/kakaoMap/MypageKakaoMap.jsx
+++ src/components/common/kakaoMap/MypageKakaoMap.jsx
@@ -11,7 +11,10 @@ export default function MypageKakaoMap() {
   useEffect(() => {
     mapscript();
   }, []);
-
+  const mapOption = {
+    center: new kakao.maps.LatLng(locationObj[0].x, locationObj[0].y), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+  };
   const mapscript = () => {
     // 주소-좌표 변환 객체를 생성합니다
     const geocoder = new kakao.maps.services.Geocoder();
@@ -47,10 +50,6 @@ export default function MypageKakaoMap() {
     // 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
   };
   function setMapType(maptype) {
-    const mapOption = {
-      center: new kakao.maps.LatLng(locationObj[0].x, locationObj[0].y), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    };
     const map = new kakao.maps.Map(root.current, mapOption);
     if (maptype === 'roadmap') {
       map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
@@ -64,20 +63,12 @@ export default function MypageKakaoMap() {
   }
   // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
   function zoomIn() {
-    const mapOption = {
-      center: new kakao.maps.LatLng(locationObj[0].x, locationObj[0].y), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    };
     const map = new kakao.maps.Map(root.current, mapOption);
     map.setLevel(map.getLevel() - 1);
   }
 
   // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
   function zoomOut() {
-    const mapOption = {
-      center: new kakao.maps.LatLng(locationObj[0].x, locationObj[0].y), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    };
     const map = new kakao.maps.Map(root.current, mapOption);
     map.setLevel(map.getLevel() + 1);
   }
