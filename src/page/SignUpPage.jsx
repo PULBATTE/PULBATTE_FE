a23bf38306309ce1b421a4218622ceae22a262da
ssori <115159126+ssori0421@users.noreply.github.com>
2023-01-06 01:23:40 +0900

    DOCS: 문서 수정

--- src/page/SignUpPage.jsx
+++ src/page/SignUpPage.jsx
@@ -4,94 +4,95 @@ import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { authInstance } from '../apis/axios';
+import { postSignup } from '../apis/auth';
+
+let cnt = 0;
 
 export default function SignUpPage() {
   const formSchema = yup.object({
-    email: yup
-      .string()
-      // .required('이메일을 입력해주세요')
-      .email('이메일 형식이 아닙니다.'),
+    userId: yup.string().email('이메일 형식이 아닙니다.'),
     password: yup
       .string()
-      // .required('영문, 숫자포함 8자리를 입력해주세요')
       .min(8, '최소 8자리 이상 가능합니다')
       .max(15, '최대 15자리 까지 가능합니다')
       .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/,
-        '영문 숫자포함 8자리를 입력해주세요.',
+        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/,
+        '영문, 숫자, 특수문자를 포함해주세요',
       ),
     passwordConfirm: yup
       .string()
       .oneOf([yup.ref('password')], '비밀번호가 다릅니다.'),
-    nickname: yup
-      .string()
-      // .required('최소 2글자 이상을 입력해주세요')
-      .min(2, '최소 2글자 이상 가능합니다')
-      .max(12, '최대 12글자 까지 입니다')
-      .matches(/^[a-zA-Z0-9가-힣]+$/, '영문, 한글, 숫자만 사용 가능합니다'),
   });
+
+  // 객체 비구조화 할당 data 로 넘어오는 hookform데이터중에서 필요한 값만 받아오기 위해서
+
+  const onSubmit = async ({ userId, password }) => {
+    const res = await postSignup({ userId, password });
+    console.log(res);
+  };
 
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm({
     mode: 'onChange',
     resolver: yupResolver(formSchema),
   });
+  cnt += 1;
 
-  const onSubmit = data => console.log(data);
+  console.log(cnt);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>회원가입</h1>
-      <p>이메일</p>
-      <StInput
-        name="email"
-        placeholder="이메일을 입력해 주세요"
-        {...register('email')}
-      />
-      <StErrorMessage>
-        {errors.email && <p>{errors.email.message}</p>}
-      </StErrorMessage>
-      <p>비밀번호</p>
-      <StInput
-        type="password"
-        name="password"
-        placeholder="영문, 숫자 조합 8글자 이상"
-        {...register('password')}
-      />
-      <StErrorMessage>
-        {errors.password && <p>{errors.password.message}</p>}
-      </StErrorMessage>
-      <StInput
-        type="password"
-        name="passwordConfirm"
-        placeholder="비밀번호를 한 번 더 입력해 주세요"
-        {...register('passwordConfirm')}
-      />
-      <StErrorMessage>
-        {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
-      </StErrorMessage>
-      <p>닉네임</p>
-      <StInput
-        name="nickname"
-        placeholder="최소 2글자 이상의 별명을 입력해 주세요"
-        {...register('nickname')}
-      />
-      <StErrorMessage>{errors.nickname?.message}</StErrorMessage>
-      <StSubmitButton
-        type="submit"
-        // onClick={handleSubmit}
-        // disabled={errors || watch()}
-      >
-        회원 가입하기
-      </StSubmitButton>
-    </form>
+    <StSignUpContainer>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>회원가입</h1>
+        <p>이메일</p>
+        <StInput
+          name="userId"
+          placeholder="이메일을 입력해 주세요"
+          {...register('userId')}
+        />
+        <StErrorMessage>
+          {errors.userId && <p>{errors.userId.message}</p>}
+        </StErrorMessage>
+        <p>비밀번호</p>
+        <StInput
+          type="password"
+          name="password"
+          placeholder="영문, 숫자 조합 8글자 이상"
+          {...register('password')}
+        />
+        <StErrorMessage>
+          {errors.password && <p>{errors.password.message}</p>}
+        </StErrorMessage>
+        <StInput
+          type="password"
+          name="passwordConfirm"
+          placeholder="비밀번호를 한 번 더 입력해 주세요"
+          {...register('passwordConfirm')}
+        />
+        <StErrorMessage>
+          {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+        </StErrorMessage>
+        <StSubmitButton
+          type="submit"
+          onClick={handleSubmit(onSubmit)}
+          disabled={isSubmitting}
+        >
+          회원 가입하기
+        </StSubmitButton>
+      </form>
+    </StSignUpContainer>
   );
 }
 
+const StSignUpContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 const StInput = styled.input`
   width: 372px;
   height: 58px;
