6b2651e6bf8f2dc7ad4ae9cc90c31b29b57acb81
ssori <115159126+ssori0421@users.noreply.github.com>
2023-01-07 00:54:12 +0900

    DOCS: Router 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,6 +6,7 @@ import SignIn from '../components/signin/SignIn';
 import Home from '../components/home/Home';
 import NotFound from '../page/NotFound';
 import SignUpPage from '../page/SignUpPage';
+import CreatePost from '../page/community/CreatePost';
 
 export default function Router() {
   return (
@@ -17,6 +18,7 @@ export default function Router() {
         <Route path="*" element={<NotFound />} />
         <Route path="/api/user/kakao/callback" element={<Kakao />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/createpost" element={<CreatePost />} />
       </Routes>
     </BrowserRouter>
   );
