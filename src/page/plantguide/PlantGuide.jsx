bdd471a4a69eafa4c930864584432db9f73f52ad
minseung-gang <strause1@nate.com>
2023-01-14 01:55:36 +0900

    DOCS: import 코드 수정

--- src/page/plantguide/PlantGuide.jsx
+++ src/page/plantguide/PlantGuide.jsx
@@ -1,50 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
 import { GrFormClose } from 'react-icons/gr';
+import { format, compareAsc } from 'date-fns';
 import Button from '../../components/common/Button';
-import waterIcon from '../../assets/image/water_drop.png';
-import shineIcon from '../../assets/image/wb_sunny.png';
-import airIcon from '../../assets/image/air.png';
 import { palette } from '../../styles/palette';
-import PlantGraph from '../../components/plantguide/plantGraph';
-import PlantEnviroment from '../../components/plantguide/PlantEnviroment';
 import Modal from '../../components/common/Modal';
 import useModal from '../../hooks/useModal';
+import PlantInfo from '../../components/plantguide/PlantInfo';
+import { postPlantsInfo } from '../../apis/plantGuide';
 
 export default function PlantGuide() {
   const [modal, onChangeModalHandler] = useModal();
-  const [openModal, setOpenModal] = useState(false);
+  const plantValue = useRef();
+  const time = format(new Date(), 'yyyy-MM-dd');
 
-  const closeModalHandler = () => {
-    document.body.style.overflowY = 'visible';
+  modal
+    ? (document.body.style.overflowY = 'hidden')
+    : (document.body.style.overflowY = 'visible');
+
+  const { beginnerName } = useParams();
+
+  const onSubmitHandler = async name => {
+    const { value } = plantValue.current;
+    if (value == '') return;
+    postPlantsInfo(name, time, Number(value))
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
     onChangeModalHandler();
   };
 
-  const openModalHandler = () => {
+  const closeModal = () => {
+    plantValue.current.value = '';
     onChangeModalHandler();
-    document.body.style.overflowY = 'hidden';
   };
 
   return (
     <StPageWrapper>
-      <Modal>
-        <div>
-          <GrFormClose />
-        </div>
-        <h3>키 입력하기</h3>
-        <div>
-          <div>
+      <Modal
+        modal={modal}
+        width="400px"
+        height="300px"
+        submit={onChangeModalHandler}
+      >
+        <StContainer>
+          <StCloseButton onClick={closeModal} />
+          <h3>키 입력하기</h3>
+          <div className="modal_comment_container">
             <span>식물의 키를 입력해주세요</span>
             <span>입력한 데이터가 성장 그래프에 반영됩니다.</span>
           </div>
-          <div>
-            <div>
-              <input type="text" />
-            </div>
+          <div className="modal_input_container">
+            <input type="number" ref={plantValue} maxLength={3} />
             <span>cm만큼 컸어요</span>
           </div>
-        </div>
-        <Button size="md" background="F2F3F7" />
+          <StButton type="button" onClick={() => onSubmitHandler(beginnerName)}>
+            입력
+          </StButton>
+        </StContainer>
       </Modal>
       <StPageInner>
         <StTitle>
@@ -54,70 +67,12 @@ export default function PlantGuide() {
             size="sm"
             color="#fff"
             width="fit-content"
-            onClick={() => onChangeModalHandler()}
+            click={onChangeModalHandler}
           >
             키 입력하기
           </Button>
         </StTitle>
-        <StContent>
-          <div className="comment_message">
-            식물을 직접 키우며 성장을 입력하고 성장그래프 가이드와 비교해보세요
-          </div>
-          <div>
-            <StGrid>
-              <div className="image_container">
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReOF8aynNvcPEJjrdeaurtuSRUcRSaW8VDDw&usqp=CAU"
-                  alt="식물이미지"
-                />
-                <span className="plant_name">스파티필름</span>
-              </div>
-              <div className="graph_container">
-                <PlantGraph />
-              </div>
-              <StGridInner>
-                <PlantEnviroment
-                  title="빛"
-                  src={shineIcon}
-                  checkPoint="4"
-                  name="sunny"
-                />
-                <PlantEnviroment
-                  title="물"
-                  src={waterIcon}
-                  checkPoint="2"
-                  name="water"
-                />
-                <PlantEnviroment
-                  title="통풍"
-                  src={airIcon}
-                  checkPoint="3"
-                  name="air"
-                />
-              </StGridInner>
-              <StTipContainer>
-                <h4>TIP</h4>
-                <ul>
-                  <li>
-                    <span>∙ 비교적 꽃을 피우기 쉬운 식물이에요 </span>
-                  </li>
-                  <li>
-                    <span>
-                      ∙ 해가 적은 곳에서도 잘 자라요. 쨍한 직사광선은 피해
-                      주세요
-                    </span>
-                  </li>
-                  <li>
-                    <span>
-                      ∙ 독성이 있어요! 반려동물이나 어린 아이에게 닿지 않는 곳에
-                      배치해 주세요
-                    </span>
-                  </li>
-                </ul>
-              </StTipContainer>
-            </StGrid>
-          </div>
-        </StContent>
+        <PlantInfo />
       </StPageInner>
     </StPageWrapper>
   );
@@ -132,91 +87,95 @@ const StTitle = styled.div`
   position: relative;
   h3 {
     font-size: 2.1rem;
+    @media (max-width: 768px) {
+      font-size: 1.4rem;
+    }
   }
   button {
     float: right;
     position: absolute;
     right: 0;
     top: 50%;
-    transform: translateY(-50%);
+    transform: translateY(-53%);
     color: #fff;
     font-weight: 600;
     border-radius: 30px;
+    @media (max-width: 768px) {
+      font-size: 0.7rem;
+    }
   }
 `;
 const StPageInner = styled.div`
   margin-top: 3rem;
 `;
 
-const StContent = styled.div`
-  margin-top: 50px;
+const StContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 40px 0;
-  .comment_message {
-    text-align: center;
-    font-size: 1.1rem;
-    color: ${palette.textColor1};
-  }
-`;
+  width: 100%;
+  height: inherit;
 
-const StGrid = styled.div`
-  display: grid;
-  grid-template-columns: 300px 1.5fr;
-  gap: 15px;
-  .image_container {
-    position: relative;
-
-    img {
-      width: 100%;
-      border-radius: 20px;
-      aspect-ratio: 1/1;
-    }
-  }
-  .plant_name {
-    position: absolute;
-    left: 20px;
-    bottom: 20px;
-    font-size: 1.4rem;
-    font-weight: 800;
-    color: #fff;
-    text-shadow: 0 0 6px rgba(0, 0, 0, 0.5);
-  }
-  .graph_container {
-    display: flex;
-    align-items: center;
-  }
-`;
-const StGridInner = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #f8f8f8;
-  border-radius: 24px;
-  gap: 0 20px;
-`;
-
-const StTipContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 15px;
   box-sizing: border-box;
-  background: #ebf1ec;
-  border-radius: 24px;
-  gap: 5px 0;
-  h4 {
-    color: #0ba47f;
-    font-size: 20px;
-    margin: 7px 0;
+  justify-content: center;
+  align-items: center;
+  gap: 25px 0;
+  h3 {
+    font-size: 1.2rem;
+    margin: 0;
   }
-  ul {
-    padding: 0;
+  .modal_comment_container {
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: 5px 0;
     span {
-      font-weight: 500;
+      color: #a3a3a3;
     }
   }
+  .modal_input_container {
+    display: flex;
+    align-items: center;
+    gap: 0 5px;
+    span {
+      color: #777777;
+    }
+    input {
+      width: 4rem;
+      padding: 5px;
+      box-sizing: border-box;
+      border: none;
+      border-bottom: 1px solid black;
+      outline: none;
+      font-size: 1.2rem;
+      font-weight: 600;
+      text-align: center;
+      &::-webkit-inner-spin-button {
+        appearance: none;
+        -moz-appearance: none;
+        -webkit-appearance: none;
+      }
+    }
+  }
+`;
+const StButton = styled.button`
+  padding: 7px 15px;
+  width: 100px;
+  border: none;
+  border-radius: 18px;
+  background: ${palette.mainColor};
+  color: #fff;
+  font-weight: 600;
+  margin-top: 15px;
+  cursor: pointer;
+  &:active {
+    background: #337461;
+  }
+`;
+const StCloseButton = styled(GrFormClose)`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+
+  font-size: 1.5rem;
+  cursor: pointer;
 `;
