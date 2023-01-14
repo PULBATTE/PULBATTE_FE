5d0c1e34fd7a722faa701439d611214e2427ba21
minseung-gang <strause1@nate.com>
2023-01-14 22:22:54 +0900

    DOCS: 네비 click관련 useState 위치 수정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,17 +1,41 @@
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
 import React, { useState, useEffect, useRef } from 'react';
 import { SlTag } from 'react-icons/sl';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import BrowserHeader from './BrowserHeader';
 import MobileHeader from './MobileHeader';
 import { palette } from '../../../styles/palette';
 
 export default function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
+  const [isReset, setIsReset] = useState(true);
+  /*  console.log(isLogin); */
+  const location = useLocation();
+  useEffect(() => {
+    const token = localStorage.getItem('Token');
+
+    setIsLogin(!!token);
+  }, [location]);
+
   return (
     <StHeader>
       <div className="header_inner">
-        <BrowserHeader />
-        <MobileHeader />
+        <BrowserHeader
+          isLogin={isLogin}
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+          isReset={isReset}
+          setIsReset={setIsReset}
+        />
+        <MobileHeader
+          isLogin={isLogin}
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+          isReset={isReset}
+          setIsReset={setIsReset}
+        />
       </div>
     </StHeader>
   );
