d8f583d29f25c1accf4c8847d3c70e19b310f715
minseung-gang <strause1@nate.com>
2023-01-15 21:43:05 +0900

    DOCS:카카오 라우터 관련 경로 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../components/common/header/Header';
-import Kakao from '../page/signin/Kakao';
-import SignIn from '../page/signin/SignIn';
+import Kakao from '../page/sign/Kakao';
+import SignIn from '../page/sign/SignIn';
 import Home from '../page/home/Home';
 import NotFound from '../page/NotFound';
-import SignUpPage from '../page/SignUpPage';
+import SignUpPage from '../page/sign/SignUpPage';
 import CreatePost from '../page/community/CreatePost';
 import PlantSearch from '../page/search/PlantSearch';
 import AddPlant from '../page/plantdiary/AddPlant';
@@ -21,7 +21,7 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/api/user/signin" element={<SignIn />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="api/user/kakao/callback" element={<Kakao />} />
+        <Route path="/api/user/kakao/callback" element={<Kakao />} />
         <Route path="/api/user/signup" element={<SignUpPage />} />
         <Route path="/createpost" element={<CreatePost />} />
         <Route path="/api/plants" element={<PlantSearch />} />
