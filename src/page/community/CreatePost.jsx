69d42ef5e297e18efcec7b64a485a7c53ab7017e
ssori <115159126+ssori0421@users.noreply.github.com>
2023-01-15 21:57:10 +0900

    FEAT: 게시물 작성 이미지 업로드 기능 구현

--- src/page/community/CreatePost.jsx
+++ src/page/community/CreatePost.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { createRef, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { ChevronLeft } from '../../assets/svgs';
+import { palette } from '../../styles/palette';
+import { createPostApi } from '../../apis/community';
 
 export default function CreatePost() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [tag, setTag] = useState('');
+  const [imgSrc, setImgSrc] = useState({
+    preview: undefined,
+    upload: '',
+  });
+  const navigate = useNavigate();
+
+  const imgInputRef = useRef(null);
+
+  console.log({ imgInputRef });
   const onChangeTitleHandler = e => {
     setTitle(e.target.value);
   };
@@ -15,28 +27,33 @@ export default function CreatePost() {
   // button value : Qa, 자랑, etc, 추천
   const onTagHandler = e => {
     e.preventDefault();
-    console.log(e.target.value);
     setTag(e.target.value);
   };
-  const onSubmitHandler = e => {
+  const onUploadImgHandler = () => {
+    console.log({ imgInputRef });
+    setImgSrc({
+      upload: imgInputRef.current.files[0],
+      preview: URL.createObjectURL(imgInputRef.current.files[0]),
+    });
+  };
+  const onSubmitHandler = async e => {
     e.preventDefault();
+    const formData = new FormData();
     const request = {
-      // title: title,
-      // content: content,
-      // tag: tag,
       title,
       content,
       tag,
     };
-    // const img = {
-    //   image: 'url',
-    // };
-    // axios.post('api.pull.com', {
-    //   request
-    //   img: {},
-    // }
-    // );
     console.log(request);
+    const blob = new Blob([JSON.stringify(request)], {
+      type: 'application/json',
+    });
+    console.log(blob);
+    formData.append('image', blob);
+    imgSrc.upload && formData.append('multipartFile', imgSrc.upload);
+    const res = await createPostApi(formData);
+    console.log(res);
+    navigate('/');
   };
   return (
     <StCreateContainer>
@@ -70,12 +87,33 @@ export default function CreatePost() {
           value={title}
           onChange={onChangeTitleHandler}
         />
-        <StContentarea
-          placeholder="내용을 작성해 주세요"
-          value={content}
-          onChange={onChangeContentHandler}
-        />
-        {/* <StTag>+ 사진 추가</StTag> */}
+        <div>
+          <StContentArea
+            placeholder="내용을 작성해 주세요"
+            value={content}
+            onChange={onChangeContentHandler}
+          />
+        </div>
+        <label>
+          <StUploadInputPText>+ 사진 업로드</StUploadInputPText>
+          <input
+            hidden
+            ref={imgInputRef}
+            type="file"
+            onChange={onUploadImgHandler}
+          />
+        </label>
+        {imgSrc.preview && (
+          <StUploadImgWrapper>
+            <StPrevImg
+              className="profile_image"
+              src={imgSrc.preview}
+              name="uploadImg"
+              alt="uploadImg"
+            />
+          </StUploadImgWrapper>
+        )}
+
         <StSubmitButton onClick={onSubmitHandler}>글 등록하기</StSubmitButton>
       </form>
     </StCreateContainer>
@@ -86,6 +124,7 @@ const StCreateContainer = styled.div`
   margin: 0 auto;
   padding: 0px 50px;
   margin-top: 84px;
+  margin-bottom: 84px;
   width: 1280px;
   h3 {
     font-size: 30px;
@@ -143,7 +182,7 @@ const StTitleArea = styled.textarea`
   border-bottom: solid 1px;
   margin-top: 80px;
 `;
-const StContentarea = styled.textarea`
+const StContentArea = styled.textarea`
   display: block;
   width: 100%;
   height: 600px;
@@ -155,9 +194,9 @@ const StContentarea = styled.textarea`
   border: none;
   border-bottom: solid 1px;
 `;
+
 const StSubmitButton = styled.button`
   background-color: #47ad8e;
-  /* display: flex; */
   width: 308px;
   height: 72px;
   border: none;
@@ -165,4 +204,32 @@ const StSubmitButton = styled.button`
   color: white;
   margin: auto;
   display: block;
+`;
+
+const StUploadImgWrapper = styled.div`
+  width: 100%;
+  padding: 24px;
+  height: 200px;
+  background-color: ${palette.lightGray};
+  border-radius: 8px;
+  margin-bottom: 24px;
+`;
+
+const StUploadInputPText = styled.p`
+  background-color: #bacdc6;
+  font-size: 14px;
+  color: #fff;
+  border-radius: 30px;
+  font-weight: bold;
+  border: none;
+  padding: 4px 8px;
+  width: 112px;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StPrevImg = styled.img`
+  height: 100%;
 `;
