d89df3382b22c9efb3145bd079fded36cea53bdc
minseung-gang <strause1@nate.com>
2023-01-15 21:39:02 +0900

    DOCS:react-Cookie 사용을 위한 import 설정

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
+import { CookiesProvider } from 'react-cookie';
 import store from './redux/store';
-import Router from './router/Router';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <Router />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </Provider>,
 );
