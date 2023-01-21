2005b6f4d63851dc03048e95bdf06634104ba3b5
minseung-gang <strause1@nate.com>
2023-01-21 09:38:09 +0900

    DESIGN: 스타일 컴포넌트 수정

--- src/components/community/Tag.jsx
+++ src/components/community/Tag.jsx
@@ -10,15 +10,16 @@ export default function Tag({ active, value, onClick, children }) {
 }
 const StTagButton = styled.button`
   background-color: ${props =>
-    props.active ? palette.mainColor : palette.lightGray};
+    props.active ? palette.borderColor4 : palette.white};
   color: ${palette.textColor1};
   color: ${props => (props.active ? palette.white : palette.textColor1)};
   font-weight: ${props => (props.active ? 'bold' : 'unset')};
   font-weight: bold;
   font-size: 14px;
   border-radius: 30px;
-  border: none;
+  border: 1px solid ${palette.borderColor4};
   padding: 4px 8px;
   width: 112px;
+  cursor: pointer;
   height: 36px;
 `;
