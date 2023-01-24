5d93eb8003bd29feaeb2c63823c45a8eec41c2e4
minseung-gang <strause1@nate.com>
2023-01-24 14:13:17 +0900

    chore: 코드 테스트 중

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -5,22 +5,19 @@ import { Cookies } from 'react-cookie';
 import { jwtUtils } from '../util/jwtUtils';
 
 // eslint-disable-next-line consistent-return
-export default function PrivateRoute(redirectPath, community) {
+export default function PrivateRoute(redirectPath, currentPath) {
   // 넘어갈 path , redirect path
 
   const cookies = new Cookies();
   const token = cookies.get('Token');
-  console.log(token);
-
-  console.log(redirectPath);
 
   // redirectUrl은 로그인이 성공후 돌아갈 화면
   console.log(jwtUtils.isAuth(token));
-  if (!jwtUtils.isAuth(token) && community) {
-    window.location.href = `/api/user/signin?redirectUrl=${redirectPath}`;
+  if (!jwtUtils.isAuth(token) && currentPath) {
+    window.location.href = `/community/geust`;
   }
-  if (jwtUtils.isAuth(token) && community) {
-    window.location.href = `/api/user/signin?redirectUrl=${redirectPath}`;
+  if (jwtUtils.isAuth(token) && currentPath) {
+    window.location.href = `/community/user`;
   }
   if (!jwtUtils.isAuth(token)) {
     alert('로그인이 필요한 페이지입니다');
