647c3f70b81b936b433f3985db32faae21b28f4a
ssori <115159126+ssori0421@users.noreply.github.com>
2023-01-25 23:38:19 +0900

    Feat: 식물 다이어리 페이지 api 추가

--- src/apis/plantDiary.js
+++ src/apis/plantDiary.js
@@ -34,8 +34,67 @@ export const createPlantJournal = async formData => {
 
 export const doneDdayCheck = async (plantJournalId, clickTag) => {
   try {
-    const data = await authInstance.post(
+    const temp = await authInstance.post(
       `/api/plantjorunal/${plantJournalId}/click/${clickTag}`,
+    );
+    const data = JSON.stringify(temp);
+    return data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const getPlantDiaryList = async () => {
+  try {
+    const data = await authInstance.get('/api/plantjournal/diarys');
+    return data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+// 실제로 함수가 호출될 때 넘기는 변수값을 인수라고 정의. 매개변수를 순서대로 넘겨받기 때문에 이름은 뭐든 상관 없다.
+export const postPlantDiary = async (plantJournalId, diaryContent) => {
+  try {
+    console.log(plantJournalId, diaryContent);
+    const data = await authInstance.post(
+      `/api/plantjournal/diary/${plantJournalId}`,
+      {
+        content: diaryContent,
+      },
+    );
+    return data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const getPlantDiary = async (plantjournalid, plantjournaldiaryid) => {
+  try {
+    const data = await authInstance.get(
+      `/api/plantjournal/diary/${plantjournalid}/${plantjournaldiaryid}`,
+    );
+    return data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const putPlantDiary = async (plantjournalid, plantjournaldiaryid) => {
+  try {
+    const data = await authInstance.put(
+      `/api/plantjournal/diary/${plantjournalid}/${plantjournaldiaryid}`,
+    );
+    return data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+export const deletePlantDiary = async (plantjournalid, plantjournaldiaryid) => {
+  try {
+    const data = await authInstance.delete(
+      `/api/plantjournal/diary/${plantjournalid}/${plantjournaldiaryid}`,
     );
     return data;
   } catch (error) {
