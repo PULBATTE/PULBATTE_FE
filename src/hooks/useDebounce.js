37e8f984e488bab88ef6a910065a0cd9ef17a61b
minseung-gang <strause1@nate.com>
2023-01-31 01:09:09 +0900

    FEAT: 디바운싱 기능구현하는 커스텀 훅 추가

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default function useDebounce(callback, delay) {
+  let timer;
+
+  return value => {
+    // 실행한 함수(setTimeout())를 취소
+    clearTimeout(timer);
+    // delay가 지나면 callback 함수를 실행
+
+    timer = setTimeout(() => callback(value), delay);
+  };
+}
