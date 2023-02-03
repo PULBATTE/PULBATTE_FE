112503e39b96fd91da3c988aff4f836e9b6c53d0
minseung-gang <strause1@nate.com>
2023-02-03 11:43:45 +0900

    FIX : 마이페이지 클릭 시 페이지 이동 추가

--- src/components/mypage/MyBoardList.jsx
+++ src/components/mypage/MyBoardList.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { palette } from '../../styles/palette';
 
 export default function MyBoardList({ post }) {
-  const { title, content, likeCnt, image, commentCnt } = post;
-
+  const { id, title, content, likeCnt, image, commentCnt } = post;
+  const navigate = useNavigate();
   return (
     <>
       <div className="content_container">
         <StPostTextBox>
-          <span className="board_title">{title}</span>
+          <span
+            className="board_title"
+            onClick={() => navigate(`/donepost/${id}`)}
+            aria-hidden="true"
+          >
+            {title}
+          </span>
           <span className="board_content">{content}</span>
         </StPostTextBox>
         {image && <img src={image} alt="게시글 이미지" className="board_img" />}
@@ -37,6 +44,11 @@ const StPostTextBox = styled.div`
   .board_title {
     font-size: 1.4rem;
     font-weight: 600;
+    cursor: pointer;
+    color: ${palette.text.black_e1};
+    &:hover {
+      text-decoration: underline;
+    }
     @media (max-width: 768px) {
       font-size: 0.9rem;
     }
