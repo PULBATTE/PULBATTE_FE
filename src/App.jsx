08596fd828b7dcaf57bfaa56736a7edca2fa084f
minseung-gang <strause1@nate.com>
2023-02-09 18:36:11 +0900

    FEAT: ToastContainer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-/* eslint import/newline-after-import: "off" */
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { ToastContainer } from 'react-toastify';
 import Router from './router/Router';
+import Header from './components/common/header/Header';
 
 const queryClient = new QueryClient();
-
 export default function App() {
   return (
     <div>
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Router />
+        <ToastContainer
+          position="bottom-center" // 알람 위치 지정
+          closeOnClick // 클릭으로 알람 닫기
+          rtl={false} // 알림 좌우 반전
+          theme="colored"
+          limit={1} // 알람 개수 제한
+          autoClose={3000}
+        />
       </QueryClientProvider>
     </div>
   );
